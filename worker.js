f6dac46382ff1398ea93080c5cc2028bb3db147b
slahssou <soufianelwo@gmail.com>
2026-05-08 03:36:40 +0200

    worker

--- worker.js
+++ worker.js
@@ -1,26 +1,20 @@
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
-
-        // Page d'accueil
-        if (url.pathname === '/' || url.pathname === '') {
-            return env.ASSETS.fetch(
-                new Request('https://assets.local/index.html', request)
-            );
-        }
+        const path = url.pathname;
 
         // Pages jeux : /game/mahjong  /game/index.html
         if (
-            url.pathname.startsWith('/game/') &&
-            url.pathname !== '/game/' &&
-            !url.pathname.match(/\.[a-zA-Z0-9]+$/)
+            path.startsWith('/game/') &&
+            path !== '/game/' &&
+            !path.match(/\.[a-zA-Z0-9]+$/)
         ) {
             return env.ASSETS.fetch(
                 new Request('https://assets.local/game/index.html', request)
             );
         }
 
-        // Tout le reste comportement normal
+        // Tout le reste  comportement normal
         return env.ASSETS.fetch(request);
     }
 }
